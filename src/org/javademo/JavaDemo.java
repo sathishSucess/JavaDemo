44a8ebe95932de37267adcb160dafa6b41f16da6
sathishSucess <sathishmec97@outlook.com>
2022-09-05 13:48:18 +0530

    commiting for push

---
+++ src/org/javademo/JavaDemo.java
@@ -0,0 +1,20 @@
+package org.javademo;
+
+public class JavaDemo {
+	 public static void main(String[] args) {
+
+	        int number = 371, originalNumber, remainder, result = 0;
+
+	        originalNumber = number;
+
+	        while (originalNumber != 0)
+	        {
+	            remainder = originalNumber % 10;
+	            result += Math.pow(remainder, 3);
+	            originalNumber /= 10;
+	        }
+
+	      
+	    }
+
+}
